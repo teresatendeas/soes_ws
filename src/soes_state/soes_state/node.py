2de9f1fb803f26d3c7d30eb41e4883ff87acdf12
Teresa <149065451+teresatendeas@users.noreply.github.com>
2025-12-09 16:36:04 +0700

    add log for camera

--- src/soes_state/soes_state/node.py
+++ src/soes_state/soes_state/node.py
@@ -291,7 +291,7 @@ class StateNode(Node):
         # CAMERA LOGIC
         # ===============================
         elif self.phase == Phase.CAMERA:
-            # tidak log "CAMERA Phase ON" setiap tick lagi
+            self.get_logger().info("CAMERA Log ON")
 
             # 1. A result arrived
             if self.vision_done is not None:
